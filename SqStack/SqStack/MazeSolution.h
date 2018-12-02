98262d238a841c49d5f4cb57bfc18771bc2eff4d
lyw <2354640364@qq.com>
2018-12-02 09:09:58 +0800

    顺序栈（迷宫待续）

--- SqStack/SqStack/MazeSolution.h
+++ SqStack/SqStack/MazeSolution.h
@@ -1,1 +1,18 @@
 #pragma once
+class Maze
+{
+public:
+	class MazeType
+	{
+		char **maze;
+		int **footprint;
+		int rowNum;
+		int colNum;
+	};
+	class PointType
+	{
+		int x;
+		int y;
+
+	};
+};
